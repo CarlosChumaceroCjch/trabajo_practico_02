503e32ff5322992b83353081caa7cbdc8aaae4f4
cjch2 <cjch2@DESKTOP-R90GMLH>
2024-04-25 17:26:00 -0300

    Declaracion de varaibles (Sin enum)

--- src/ar/edu/unju/fi/ejercicio01/model/Producto.java
+++ src/ar/edu/unju/fi/ejercicio01/model/Producto.java
@@ -1,5 +1,39 @@
 package ar.edu.unju.fi.ejercicio01.model;
 
 public class Producto {
+	private String cod;
+	private String desc;
+	private double precioU;
+	
+	public Producto() {
+		
+	}
 
+	public String getCod() {
+		return cod;
+	}
+
+	public void setCod(String cod) {
+		this.cod = cod;
+	}
+
+	public String getDesc() {
+		return desc;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public double getPrecioU() {
+		return precioU;
+	}
+
+	public void setPrecioU(double precioU) {
+		this.precioU = precioU;
+	}
+	
+	
+	
+	
 }
